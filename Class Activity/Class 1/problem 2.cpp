b976f6d819a1936c8564261a8279c0b2144a91cf
Md. Ammar Hossain <mhossain221601@bscse.uiu.ac.bd>
2026-05-19 01:37:23 +0600

    two problem added

---
+++ Class Activity/Class 1/problem 2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    while(true)
+    {
+        char input;
+        cin >> input;
+        if(input!='A')
+            cout << input << endl;
+        else
+            break;
+    }
+
+    return 0;
+}
